ddd1ce0cca3e5acabb5a1fe23db30cbdc02be35d
emilio <emilio113kariuki@gmail.com>
2022-03-19 00:45:50 +0300

    March 19, 2022 12:45 AM

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,7 +6,7 @@ import 'package:motivation/Api.dart';
 import 'package:motivation/model.dart';
 
 Future<User> getQuote() async {
-    final response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts/1"));
+    final response = await http.get(Uri.parse("https://zenquotes.io/api/quotes"));
 
     if (response.statusCode == 200) {
       // If the server returns an OK response, then parse the JSON.
