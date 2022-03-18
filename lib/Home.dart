e256ca4004850c34022b9b979f612ec5db2b0e23
emilio <emilio113kariuki@gmail.com>
2022-03-19 00:49:51 +0300

    March 19, 2022 12:49 AM

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,16 +6,20 @@ import 'package:motivation/Api.dart';
 import 'package:motivation/model.dart';
 
 Future<User> getQuote() async {
-    final response = await http.get(Uri.parse("https://zenquotes.io/api/quotes"));
+  final response = await http.get(Uri.parse("https://zenquotes.io/api/quotes"));
 
-    if (response.statusCode == 200) {
-      // If the server returns an OK response, then parse the JSON.
-      return User.fromJson(json.decode(response.body));
-    } else {
-      // If the response was umexpected, throw an error.
-      throw Exception('Failed to load post');
-    }
+  if (response.statusCode == 200) {
+    // If the server returns an OK response, then parse the JSON.
+    return User.fromJson(json.decode(response.body));
+    // var extractedData =
+    //     List<Map<String, dynamic>>.from(jsonDecode(response.body));
+    // return extractedData;
+  } else {
+    // If the response was umexpected, throw an error.
+    throw Exception('Failed to load post');
   }
+}
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -28,7 +32,7 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    futureAlbum = getQuote() ;
+    futureAlbum = getQuote();
   }
 
   @override
@@ -42,7 +46,7 @@ class _HomeState extends State<Home> {
           future: futureAlbum,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return Text(snapshot.data!.q);  
+              return Text(snapshot.data!.q);
             } else if (snapshot.hasError) {
               return Text('${snapshot.error}');
             }
