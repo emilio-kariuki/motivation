88ed8733b5587c327b8e7ff03d15955c685b1b8d
emilio <emilio113kariuki@gmail.com>
2022-03-18 23:57:52 +0300

    March 18, 2022 11:57 PM

--- lib/model.dart
+++ lib/model.dart
@@ -4,10 +4,6 @@
 
 import 'dart:convert';
 
-// List<User> userFromJson(String str) => List<User>.from(json.decode(str).map((x) => User.fromJson(x)));
-
-// String userToJson(List<User> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
-
 class User {
     User({
         required this.q,
