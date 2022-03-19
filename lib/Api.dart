857a59d05543ea24a02c8dc37e726e4cf2dec3b2
emilio <emilio113kariuki@gmail.com>
2022-03-19 11:13:18 +0300

    March 19, 2022 11:13 AM

--- lib/Api.dart
+++ lib/Api.dart
@@ -6,12 +6,13 @@ import 'package:motivation/model.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  static Future<List<Map<String, dynamic>>> getQuote() async {
+  static Future<User> getQuote() async {
     final response = await http.get(Uri.parse("https://zenquotes.io/api/quotes"));
 
     if (response.statusCode == 200) {
       // If the server returns an OK response, then parse the JSON.
-      return List<Map<String, dynamic>>.from(json.decode(response.body));
+      
+      return User.fromJson(json.decode(response.body));
     } else {
       // If the response was umexpected, throw an error.
       throw Exception('Failed to load post');
