82df6ddb60d26e10c5c6d8cca54dc269ab42445d
emilio <emilio113kariuki@gmail.com>
2022-03-19 13:02:20 +0300

    March 19, 2022 1:02 PM

--- lib/Api.dart
+++ lib/Api.dart
@@ -7,26 +7,15 @@ import 'package:motivation/model.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<List<User>> getQuote() async {
+  void getQuote() async {
     final response =
         await http.get(Uri.parse("https://zenquotes.io/api/quotes"));
     List<User> postList = <User>[];
 
     if (response.statusCode == 200) {
       // If the call to the server was successful, parse the JSON
-      List<dynamic> values = <dynamic>[];
-      values = json.decode(response.body);
-      if (values.length > 0) {
-        for (int i = 0; i < values.length; i++) {
-          if (values[i] != null) {
-            Map<String, dynamic> map = values[i];
-            postList.add(User.fromJson(map));
-            debugPrint('Id-------${map['id']}');
-          }
-        }
-      }
-      print("THis is postlist $postList");
-      return postList;
+      var jsonString = response.body;
+      var jsonMap = json.decode(jsonString);
     } else {
       throw Exception('Failed to load post');
     }
