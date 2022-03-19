c2360bac6aa10772850a286d4429f8fd677dc345
emilio <emilio113kariuki@gmail.com>
2022-03-19 13:05:52 +0300

    March 19, 2022 1:05 PM

--- lib/Api.dart
+++ lib/Api.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_import, deprecated_member_use
+// ignore_for_file: unused_import, deprecated_member_use, unused_local_variable, avoid_init_to_null
 
 import 'dart:convert';
 import 'package:dio/dio.dart';
@@ -7,16 +7,17 @@ import 'package:motivation/model.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  void getQuote() async {
+  Future<User> getQuote() async {
     final response =
         await http.get(Uri.parse("https://zenquotes.io/api/quotes"));
-    List<User> postList = <User>[];
+    var userModel = null;
 
     if (response.statusCode == 200) {
       // If the call to the server was successful, parse the JSON
       var jsonString = response.body;
       var jsonMap = json.decode(jsonString);
-      User.fromJson(jsonMap);
+      userModel = User.fromJson(jsonMap);
+      return userModel;
     } else {
       throw Exception('Failed to load post');
     }
