a58a68e1a1730910f7764957b533f90b9e6e473b
emilio <emilio113kariuki@gmail.com>
2022-03-19 14:47:01 +0300

    March 19, 2022 2:47 PM

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:motivation/Api.dart';
@@ -33,6 +34,7 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Fetch Data Example'),
@@ -44,15 +46,25 @@ class _HomeState extends State<Home> {
             child: ListView.builder(
                   itemCount: _model?.length,
                   itemBuilder: (context, index) {
-            return Container(
-              height: 60,
-              child: Column(
-                children: [
-                  Text(_model![index].q,),
-                  Text(_model![index].a,),
-                ],
+            return Padding(
+              padding: const EdgeInsets.only(left: 8,right: 8, bottom: 5),
+              child: Container(
+                padding : const EdgeInsets.all(10),
+                // ignore: prefer_const_constructors
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 2, color: Color.fromARGB(255, 19, 15, 26)),
+                  color: Colors.grey,
+                ),
+                height: size.height * 0.15,
+                child: Column(
+                  children: [
+                    Text(_model![index].q,style: GoogleFonts.roboto(fontSize: 20, color: Colors.black)),
+                    Text(_model![index].a,),
+                  ],
+                ),
+                // color: Colors.red,
               ),
-              // color: Colors.red,
             );
                   },
                 ),
